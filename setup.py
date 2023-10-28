0661de157d3559f43f77977e07b65e736bacc079
= <=>
2023-10-28 21:10:50 +0530

    Added setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,15 @@ setup(
   description = 'Your go-to library for decentralised learning',   # Give a short description about your library
   author = 'SVM',                   # Type in your name
   author_email = 'shouryagoel10000@gmail.com',      # Type in your E-Mail
-  url = '',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  url = 'https://github.com/VasuK111/fedle',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/VasuK111/fedle/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  keywords = ['Deep Learning', 'Federated Learning', 'Decentralisation', 'web3','open source'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+        'flwr>=1.0, <2.0',
+        'torch==1.13.1',
+        'torchvision==0.14.1'
+        'validators==0.18.2',
+        'beautifulsoup4',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
