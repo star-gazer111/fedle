f7722f2fd0634a36e09feadaff3a5898522e6fdb
Sh0g0-1758 <shouryagoel10000@gmail.com>
2023-10-29 11:00:31 +0530

    Added Auth and real time contract interaction

--- github.js
+++ github.js
@@ -3,8 +3,7 @@ const EMAIL = 'shouryagoel10000@gmail.com'
 // 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
 const github = require('octokat')({ token: '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' })
 
-return github.fromUrl(`https://api.github.com/users/${USER}/events/public`) // https://api.github.com/users/<username>/events/public
-
+return github.fromUrl(`https://api.github.com/users/${USER}/events/public`)
     .fetch()
     .then(events => {
         console.log(events.items[0].payload.commits[0].sha);
